5a67babb98605f8093f7a79a15c7166e95e42adc
john <john.makaronas@protonmail.com>
2024-06-30 03:44:37 +0300

    types

---
+++ internal/app/compman/types/types.go
@@ -0,0 +1,74 @@
+package types
+
+import "encoding/json"
+
+type User struct {
+	ID       string
+	Username string
+	Email    string
+	Password string
+}
+
+type CompanyType int
+
+const (
+	CompanyTypeCorporation CompanyType = iota
+	CompanyTypeNonProfit
+	CompanyTypeCooperative
+	CompanyTypeSoleProprietorship
+)
+
+func (c CompanyType) String() string {
+	var r string
+	switch c {
+	case CompanyTypeCorporation:
+		r = "corporation"
+	case CompanyTypeNonProfit:
+		r = "non-profit"
+	case CompanyTypeCooperative:
+		r = "cooperative"
+	case CompanyTypeSoleProprietorship:
+		r = "sole-proprietorship"
+	default:
+		r = ""
+	}
+	return r
+}
+
+func ParseCompanyType(str string) CompanyType {
+	var r CompanyType
+	switch str {
+	case "corporation":
+		r = CompanyTypeCorporation
+	case "non-profit":
+		r = CompanyTypeNonProfit
+	case "cooperative":
+		r = CompanyTypeCooperative
+	case "sole-proprietorship":
+		r = CompanyTypeSoleProprietorship
+	default:
+		r = -1
+	}
+	return r
+}
+func (c CompanyType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.String())
+}
+
+func (c *CompanyType) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := json.Unmarshal(data, &s); err != nil {
+		return err
+	}
+	*c = ParseCompanyType(s)
+	return nil
+}
+
+type Company struct {
+	ID          string      `json:"id"`
+	Name        string      `json:"name"`
+	Desc        string      `json:"description"`
+	EmployeeCnt int         `json:"employee_count"`
+	Registered  bool        `json:"registered"`
+	CType       CompanyType `json:"type"`
+}
